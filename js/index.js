c86d1798c7b8e3b330fc89d0df6bb5c022297667
Nguyễn Hải Nam <59411728+NguyenHaiNam24082000@users.noreply.github.com>
2020-08-25 14:41:16 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,51 @@
 const heading= document.querySelector("h1");
 window.addEventListener("keydown",function(e)
 {
-    // const audio= document.querySelector("audio") 
-    console.log(e);
-    if(e.keyCode>=65&&e.keyCode<=90)
+    if(e.keyCode!=32)
     {
         heading.innerHTML=e.key;
         heading.dataset.text=e.key;
-        setTimeout(function()
-        {
-            heading.innerHTML="Press Key";
-            heading.dataset.text="Press Key";
-        },2000)
     }
+    else
+    {
+        heading.innerHTML="Space";
+        heading.dataset.text="Space";
+    }
+    setTimeout(function()
+    {
+        heading.innerHTML="Press";
+        heading.dataset.text="Press";
+    },1500);
+});
+let mouseLeftCurrent=0;
+let mouseWheelCurrent=0;
+let mouseRightCurrent=0;
+window.addEventListener("mousedown",function(e)
+{
+    switch(e.button)
+    {
+        case 0:
+            mouseLeftCurrent++;
+            heading.innerHTML="Left "+mouseLeftCurrent;
+            heading.dataset.text="Left "+mouseLeftCurrent;
+            break;
+        case 1:
+            mouseWheelCurrent++;
+            heading.innerHTML="Wheel "+mouseWheelCurrent;
+            heading.dataset.text="Wheel "+mouseWheelCurrent;
+            break;
+        case 2:
+            mouseRightCurrent++;
+            heading.innerHTML="Right "+mouseRightCurrent;
+            heading.dataset.text="Right "+mouseRightCurrent;
+            break;
+    }
+    setTimeout(function()
+    {
+        mouseLeftCurrent=0;
+        mouseWheelCurrent=0;
+        mouseRightCurrent=0;
+        heading.innerHTML="Press";
+        heading.dataset.text="Press";
+    },7500);
 });
